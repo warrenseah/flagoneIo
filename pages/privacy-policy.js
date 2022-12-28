3865f47843e258eca5d304c97e3f1937ea27cfab
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-12-28 17:10:07 +0800

    Update privacy-policy.js

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -1,104 +1,253 @@
 import React from "react";
-import NavbarTwo from "../components/Layouts/NavbarTwo";
+import Link from "next/link";
+import SinglePageNavBarDark from "../components/Layouts/SinglePageNavbarDark";
 import PageBanner from "../components/Common/PageBanner";
 import Footer from "../components/Layouts/Footer";
 
 const PrivacyPolicy = () => {
   return (
     <>
-      <NavbarTwo />
+      <SinglePageNavBarDark />
 
-      <PageBanner pageTitle="Privacy Policy" BGImage="/images/page-banner5.jpg" />
+      <PageBanner
+        pageTitle="Privacy Policy"
+        BGImage="/images/page-banner5.jpg"
+      />
 
       <div class="text-container ptb-100">
         <div class="container">
-          <h4>What is Lorem Ipsum?</h4>
-
+          <p>Effective date: December 06, 2018</p>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            Flag One Pte Ltd (&#8220;us&#8221;, &#8220;we&#8221;, or
+            &#8220;our&#8221;) operates the FlagOne.io website (the
+            &#8220;Service&#8221;).
           </p>
-
-          <h4>Where does it come from?</h4>
+          <h4>Information Collection And Use</h4>
           <p>
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature,
-            discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
-            of Good and Evil) by Cicero, written in 45 BC. This book is a
-            treatise on the theory of ethics, very popular during the
-            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-            amet..".
+            We collect several different types of information for various
+            purposes to provide and improve our Service to you.
           </p>
-
+          <h4>Types of Data Collected</h4>
           <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            <strong>Personal Data</strong>
           </p>
-
-          <h4>Where can I get some?</h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim.
+            While using our Service, we may ask you to provide us with certain
+            personally identifiable information that can be used to contact or
+            identify you (“Personal Data”). Personally identifiable information
+            may include, but is not limited to:
+          </p>
+          <ul>
+            <li>Email address</li>
+            <li>First name and last name</li>
+            <li>Phone number</li>
+            <li>Address, State, Province, ZIP/Postal code, City</li>
+            <li>Cookies and Usage Data</li>
+          </ul>
+          <h4>Usage Data</h4>
+          <p>
+            We may also collect information how the Service is accessed and used
+            (“Usage Data”). This Usage Data may include information such as your
+            computer’s Internet Protocol address (e.g. IP address), browser
+            type, browser version, the pages of our Service that you visit, the
+            time and date of your visit, the time spent on those pages, unique
+            device identifiers and other diagnostic data.
+          </p>
+          <h4>Tracking & Cookies Data</h4>
+          <p>
+            We use cookies and similar tracking technologies to track the
+            activity on our Service and hold certain information.
           </p>
 
           <p>
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected.
+            Cookies are files with small amount of data which may include an
+            anonymous unique identifier. Cookies are sent to your browser from a
+            website and stored on your device. Tracking technologies also used
+            are beacons, tags, and scripts to collect and track information and
+            to improve and analyze our Service.
           </p>
 
           <p>
-            But I must explain to you how all this mistaken idea of denouncing
-            pleasure and praising pain was born and I will give you a complete
-            account of the system, and expound the actual teachings of the great
-            explorer of the truth, the master-builder of human happiness. No one
-            rejects, dislikes, or avoids pleasure itself, because it is
-            pleasure, but because those who do not know how to pursue pleasure
-            rationally encounter consequences that are extremely painful. Nor
-            again is there anyone who loves or pursues or desires to obtain pain
-            of itself, because it is pain, but because occasionally
-            circumstances occur in which toil and pain can procure him some
-            great pleasure. To take a trivial example, which of us ever
-            undertakes laborious physical exercise, except to obtain some
-            advantage from it? But who has any right to find fault with a man
-            who chooses to enjoy a pleasure that has no annoying consequences,
-            or one who avoids a pain that produces no resultant pleasure?
+            You can instruct your browser to refuse all cookies or to indicate
+            when a cookie is being sent. However, if you do not accept cookies,
+            you may not be able to use some portions of our Service.
+          </p>
+
+          <p>Examples of Cookies we use:</p>
+          <ul>
+            <li>
+              <strong>Session Cookies.</strong> We use Session Cookies to
+              operate our Service.
+            </li>
+            <li>
+              <strong>Preference Cookies.</strong> We use Preference Cookies to
+              remember your preferences and various settings.
+            </li>
+            <li>
+              <strong>Security Cookies.</strong> We use Security Cookies for
+              security purposes.
+            </li>
+          </ul>
+
+          <h4>Use of Data</h4>
+          <ul>
+            <li>
+              Flag One Pte Ltd uses the collected data for various purposes:
+            </li>
+            <li>To provide and maintain the Service</li>
+            <li>To notify you about changes to our Service</li>
+            <li>
+              To allow you to participate in interactive features of our Service
+              when you choose to do so
+            </li>
+            <li>To provide customer care and support</li>
+            <li>
+              To provide analysis or valuable information so that we can improve
+              the Service
+            </li>
+            <li>To monitor the usage of the Service</li>
+            <li>To detect, prevent and address technical issues</li>
+          </ul>
+
+          <h4>Tracking & Cookies Data</h4>
+          <p>
+            Your information, including Personal Data, may be transferred to —
+            and maintained on — computers located outside of your state,
+            province, country or other governmental jurisdiction where the data
+            protection laws may differ than those from your jurisdiction.
+          </p>
+          <p>
+            If you are located outside Singapore and choose to provide
+            information to us, please note that we transfer the data, including
+            Personal Data, to Singapore and process it there.
+          </p>
+          <p>
+            Your consent to this Privacy Policy followed by your submission of
+            such information represents your agreement to that transfer.
+          </p>
+          <p>
+            Flag One Pte Ltd will take all steps reasonably necessary to ensure
+            that your data is treated securely and in accordance with this
+            Privacy Policy and no transfer of your Personal Data will take place
+            to an organization or a country unless there are adequate controls
+            in place including the security of your data and other personal
+            information.
+          </p>
+          <h4>Disclosure Of Data</h4>
+          <p>
+            <strong>Legal Requirements</strong>
+          </p>
+          <p>
+            Flag One Pte Ltd may disclose your Personal Data in the good faith
+            belief that such action is necessary to:
+          </p>
+          <ul>
+            <li>To comply with a legal obligation</li>
+            <li>
+              To protect and defend the rights or property of Flag One Pte Ltd
+            </li>
+            <li>
+              To prevent or investigate possible wrongdoing in connection with
+              the Service
+            </li>
+            <li>
+              To protect the personal safety of users of the Service or the
+              public
+            </li>
+            <li>To protect against legal liability</li>
+          </ul>
+          <h4>Security Of Data</h4>
+          <p>
+            The security of your data is important to us, but remember that no
+            method of transmission over the Internet, or method of electronic
+            storage is 100% secure. While we strive to use commercially
+            acceptable means to protect your Personal Data, we cannot guarantee
+            its absolute security.
+          </p>
+
+          <h4>Service Providers</h4>
+          <p>
+            We may employ third party companies and individuals to facilitate
+            our Service (&#8220;Service Providers&#8221;), to provide the
+            Service on our behalf, to perform Service-related services or to
+            assist us in analyzing how our Service is used.
+          </p>
+          <p>
+            These third parties have access to your Personal Data only to
+            perform these tasks on our behalf and are obligated not to disclose
+            or use it for any other purpose.
+          </p>
+          <h4>Analytics</h4>
+          <p>
+            We may use third-party Service Providers to monitor and analyze the
+            use of our Service.
+          </p>
+          <p>
+            <strong>Google Analytics</strong>
+          </p>
+          <p>
+            Google Analytics is a web analytics service offered by Google that
+            tracks and reports website traffic. Google uses the data collected
+            to track and monitor the use of our Service. This data is shared
+            with other Google services. Google may use the collected data to
+            contextualize and personalize the ads of its own advertising
+            network.
+          </p>
+          <p>
+            You can opt-out of having made your activity on the Service
+            available to Google Analytics by installing the Google Analytics
+            opt-out browser add-on. The add-on prevents the Google Analytics
+            JavaScript (ga.js, analytics.js, and dc.js) from sharing information
+            with Google Analytics about visits activity.
+            <br />
+            For more information on the privacy practices of Google, please
+            visit the Google Privacy &amp; Terms web page:
+            https://policies.google.com/privacy?hl=en
+          </p>
+          <h4>Links To Other Sites</h4>
+          <p>
+            Our Service may contain links to other sites that are not operated
+            by us. If you click on a third party link, you will be directed to
+            that third party&#8217;s site. We strongly advise you to review the
+            Privacy Policy of every site you visit.
+          </p>
+          <p>
+            We have no control over and assume no responsibility for the
+            content, privacy policies or practices of any third party sites or
+            services.
+          </p>
+          <h4>Children&#8217;s Privacy</h4>
+          <p>
+            Our Service does not address anyone under the age of 18
+            (&#8220;Children&#8221;).
+          </p>
+          <p>
+            We do not knowingly collect personally identifiable information from
+            anyone under the age of 18. If you are a parent or guardian and you
+            are aware that your Children has provided us with Personal Data,
+            please contact us. If we become aware that we have collected
+            Personal Data from children without verification of parental
+            consent, we take steps to remove that information from our servers.
+          </p>
+          <h4>Changes To This Privacy Policy</h4>
+          <p>
+            We may update our Privacy Policy from time to time. We will notify
+            you of any changes by posting the new Privacy Policy on this page.
+          </p>
+          <p>
+            We will let you know via email and/or a prominent notice on our
+            Service, prior to the change becoming effective and update the
+            &#8220;effective date&#8221; at the top of this Privacy Policy.
+          </p>
+          <p>
+            You are advised to review this Privacy Policy periodically for any
+            changes. Changes to this Privacy Policy are effective when they are
+            posted on this page.
+          </p>
+          <h4>Contact Us</h4>
+          <p>
+            If you have any questions about this Privacy Policy,{" "}
+            <Link href="/contact">contact us here</Link>
           </p>
         </div>
       </div>
