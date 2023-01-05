9de85425de0d88f46027a0b52fc15ec8621c8ad3
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-01-05 18:58:50 +0800

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://flagone.io' 
+? 'https://scintillating-flan-55b237.netlify.app' 
 : 'http://localhost:3000';
 
 export default baseUrl;
