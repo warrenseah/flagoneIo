6322b6f52ff1883c4950b7d8048b2a0952f5a2a2
Renu Yadav <renuyadav@192.168.1.9>
2023-09-30 20:08:00 +0530

    compared and changed document file

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Link from "next/link";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,19 +9,19 @@ class MyDocument extends Document {
     return (
       <Html lang="zxx">
         <Head>
-          <Link
+          <link
             href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap"
             rel="stylesheet"
           />
-          <Link
+          <link
             href="https://fonts.googleapis.com/css?family=Saira:100,200,300,400,500,600,700,800,900&display=swap"
             rel="stylesheet"
           />
-          <Link
+          <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
-          <Link rel="icon" type="image/png" href="/favicon.png" />
+          <link rel="icon" type="image/png" href="/favicon.png"></link>
         </Head>
         <body>
           <Main />
